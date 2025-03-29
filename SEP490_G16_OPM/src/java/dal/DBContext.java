b2768a5cdf777e815cb74ca7ed0759308b8f9f15
twikou <eu.hi3107@gmail.com>
2025-03-29 21:06:32 +0700

    (feat) insert

--- SEP490_G16_OPM/src/java/dal/DBContext.java
+++ SEP490_G16_OPM/src/java/dal/DBContext.java
@@ -6,7 +6,9 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import util.ClassConvertor;
@@ -41,6 +43,15 @@ public class DBContext {
         return pstm;
     }
 
+    public PreparedStatement prepareStatementReturnId(String query, String id, Object... params) throws SQLException {
+        PreparedStatement pstm = connection.prepareStatement(query, new String[]{id});
+        int index = 1;
+        for (Object param : params) {
+            pstm.setObject(index++, param);
+        }
+        return pstm;
+    }
+
     public <T> List<T> fetchAll(Class<T> clazz, String query, Object... params) {
         List<T> result = new ArrayList<>();
         ClassConvertor<T> convertor = new ClassConvertor<>(clazz);
@@ -64,6 +75,41 @@ public class DBContext {
                 return item;
             }
             return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+
+    /**
+     * Thêm data và trả về ID của row vừa tạo
+     * @param <T> 
+     * @param clazz
+     * @param table Tên bảng
+     * @param id tên cột của id (ví dụ: FarmID)
+     * @param data Farm object chứa data cần tạo
+     * @param columns Tên các cột
+     * @return 
+     */
+    public <T> int insert(Class<T> clazz, String table, String id, T data, String... columns) {
+        ClassConvertor<T> convertor = new ClassConvertor<>(clazz);
+        Map<String, Object> nonNullFields = convertor.getNotNullFields(data);
+        List<String> nonNullColumns = Arrays.asList(columns).stream().filter(col -> nonNullFields.containsKey(col.toLowerCase())).toList();
+        List<Object> nonNullValues = nonNullColumns.stream().map(col -> nonNullFields.get(col.toLowerCase())).toList();
+        String query = "INSERT INTO %s ".formatted(table);
+        query += "(" + String.join(", ", nonNullColumns) + ")";
+        query += " VALUES (" + String.join(", ", "?".repeat(nonNullColumns.size()).split("")) + ")";
+        try (PreparedStatement pstm = prepareStatementReturnId(query, id, nonNullValues.toArray())) {
+            int rows = pstm.executeUpdate();
+            if (rows == 0) {
+                throw new SQLException("Insert failed");
+            }
+            ResultSet generatedKey = pstm.getGeneratedKeys();
+            if (generatedKey.next()) {
+                return generatedKey.getInt(1);
+            } else {
+                throw new SQLException("Insert failed");
+            }
         } catch (SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex.getMessage());
