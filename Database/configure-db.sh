9952b1c439c0c781b699e748b106ca64fff6365b
twikou <eu.hi3107@gmail.com>
2025-04-11 17:38:51 +0700

    (fix) docker mssql configure script

--- Database/configure-db.sh
+++ Database/configure-db.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-DBSTATUS=1
-ERRCODE=1
-i=0
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
-	DBSTATUS=$(/opt/mssql-tools18/bin/sqlcmd -h -1 -t 1 -U sa -P $MSSQL_SA_PASSWORD -C -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases");
-	ERRCODE=$?
-	echo "Database not ready yet"
-	sleep 1
+for i in {1..50};
+do
+	/opt/mssql-tools18/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -C -i OPM.sql
+	if [ $? -eq 0 ]
+	then
+		echo "OPM.sql run completed"
+		break
+	else
+		echo "Database not ready yet..."
+		sleep 1
+	fi
 done
-if [ $DBSTATUS -ne 0 ] OR [ $ERRCODE -ne 0 ]; then 
-	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
-	exit 1
-fi
-/opt/mssql-tools18/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -C -i OPM.sql
-echo "Import default data complete"
