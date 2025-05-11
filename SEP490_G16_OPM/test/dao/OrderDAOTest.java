4a8631c969ce3eb006dbac837b4d06c20bc69f81
twikou <eu.hi3107@gmail.com>
2025-05-12 02:23:10 +0700

    (test) cancel orders

--- SEP490_G16_OPM/test/dao/OrderDAOTest.java
+++ SEP490_G16_OPM/test/dao/OrderDAOTest.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.List;
+import model.Order;
 import org.junit.Test;
 
 public class OrderDAOTest {
@@ -7,10 +9,29 @@ public class OrderDAOTest {
     @Test
     public void testGetExpiredOrders() {
         var dao = new OrderDAO();
-        var expired = dao.getExpiredOrders();
-        assert !expired.isEmpty();
-        expired.forEach(order -> {
+        var orders = dao.getExpiredOrders();
+        assert !orders.isEmpty();
+        orders.forEach(order -> {
             System.out.println(order.getOrderID());
         });
+    }
+    
+    @Test
+    public void testGetOverProcessedDateOrders() {
+        var dao = new OrderDAO();
+        var orders = dao.getOverProcessedDateOrders();
+        assert !orders.isEmpty();
+        orders.forEach(order -> {
+            System.out.println(order.getOrderID());
+        });
+    }
+    
+    @Test
+    public void testCancelOrders() {
+        var dao = new OrderDAO();
+        Order order = new Order();
+        order.setOrderID(13);
+        var orders = List.of(order);
+        assert dao.cancelOrders(orders);
     }
 }
