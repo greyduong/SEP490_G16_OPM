fd070ebd94fa75d5e36c56055f379f1bbf872910
twikou <eu.hi3107@gmail.com>
2025-06-01 01:08:16 +0700

    fix: farms, farm-detail filter

--- SEP490_G16_OPM/src/java/filter/OnlyGuestAndDealerFilter.java
+++ SEP490_G16_OPM/src/java/filter/OnlyGuestAndDealerFilter.java
@@ -6,16 +6,24 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
+import model.User;
 
 @WebFilter(urlPatterns = {
+    "/farms",
+    "/farm-detail"
 })
-public class NeedLoginFilter extends HttpFilter {
+public class OnlyGuestAndDealerFilter extends HttpFilter {
+
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (request.getSession().getAttribute("user") == null) {
-            response.sendRedirect(request.getContextPath() + "/login");
+            chain.doFilter(request, response);
+            return;
+        }
+        User user = (User) request.getSession().getAttribute("user");
+        if (user.getRoleID() != 5) {
+            response.sendRedirect(request.getContextPath() + "/home?error=403");
             return;
         }
         chain.doFilter(request, response);
